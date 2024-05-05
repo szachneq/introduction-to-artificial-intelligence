33d3264b2fe8db47e967f0ca5e22defda0c93d30
Jan Szachno <j.szachno@gmail.com>
2024-05-05 12:18:51 +0200

    additional code changes for experiments

--- lab6/main.py
+++ lab6/main.py
@@ -11,6 +11,8 @@ LEARNING_RATE = 0.1
 DISCOUNT_FACTOR = 0.99
 EXPLORATION_RATE = 1
 EXPLORATION_DECAY_RATE = 0.0001
+MAX_EPISODE_LENGTH = 200
+
 SLIPPERY = True
 
 def update_q_table(q_table, current_state, next_state, action, reward):
@@ -23,6 +25,7 @@ def main():
         'FrozenLake-v1',
         map_name="8x8",
         is_slippery=SLIPPERY,
+        max_episode_steps=MAX_EPISODE_LENGTH,
     )
 
     # constants
@@ -33,8 +36,10 @@ def main():
     q = np.zeros((STATE_SPACE_SIZE, ACTION_SPACE_SIZE))
 
     episode_reward = []
+    steps_taken = []
     print('Training started')
     for i in range(NUM_EPISODES):
+        steps_taken.append(0)
         if i % 100 == 0:
             print(f'Episode {i}')
             episode_reward.append(0)
@@ -54,6 +59,7 @@ def main():
                 action = np.argmax(q[state,:])
 
             next_state, reward, terminated, truncated, _ = training_env.step(action)
+            steps_taken[i] += 1
 
             episode_reward[int(i / 100)] += reward
 
@@ -71,14 +77,36 @@ def main():
 
     training_env.close()
 
-    x = [ x for x in range(100, NUM_EPISODES+1, 100) ]
-
-    y = [ 0 for _ in range(len(episode_reward))]
+    x1 = [ x for x in range(100, NUM_EPISODES+1, 100) ]
+    y1 = [ 0 for _ in range(len(episode_reward))]
     for i in range(len(episode_reward)):
-        y[i] = episode_reward[i] / 100
+        y1[i] = episode_reward[i] / 100
 
-    plt.plot(x, y)
+    plt.plot(x1, y1)
     plt.savefig('training.png')
+    plt.clf()
+
+    x2 = [ x for x in range(100, NUM_EPISODES+1, 100) ]
+    sums = []
+    for i in range(NUM_EPISODES):
+        if i % 100 == 0:
+            sums.append(0)
+        sums[int(i / 100)] += steps_taken[i]
+    y2 = []
+    for i in range(len(sums)):
+        y2.append(sums[i] / 100)
+    plt.plot(x2, y2)
+    plt.savefig('steps_taken.png')
+    plt.clf()
+    
+    x3 = [ x for x in range(100, NUM_EPISODES+1, 100) ]
+    y3 = []
+    for i in range(len(y2)):
+        y3.append(0)
+        y3[i] = y1[i] / y2[i]
+    plt.plot(x3, y3)
+    plt.savefig('score.png')
+    plt.clf()
 
     # VISUALIZATION
     env = gym.make(
@@ -86,6 +114,7 @@ def main():
         map_name="8x8",
         is_slippery=SLIPPERY,
         render_mode="human",
+        max_episode_steps=MAX_EPISODE_LENGTH,
     )
 
     print('Visualization started')
